f7cd78ea2dd59c7a927751e06c93f3a494e8ec52
Ethan Brooks <ethanabrooks@gmail.com>
2017-04-26 15:40:12 -0400

    added files

---
+++ train.tmux.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+set -e
+logdir=ardrone
+spec_path=spec.json
+session=a3c
+docker build ~/ardrone-project/ -t ardrone
+
+while [[ $# -gt 1 ]]; do
+  key="$1"
+
+  case $key in
+      -l|--log-dir)
+        logdir="$2"
+        shift # past argument
+      ;;
+      -s|--spec-path)
+        spec_path="$2"
+        shift # past argument
+      ;;
+      -t|--target-session)
+        session="$2"
+        shift # past argument
+      ;;
+      *)
+      ;;
+  esac
+  shift # past argument or value
+done
+
+spec=$(tr -d "\n \"'" < spec.json) # remove whitespace and quotes
+num_workers=$(cat $spec_path | jq "{worker}[]|length")
+
+mkdir -p $logdir
+kill $( lsof -i:12345 -t ) > /dev/null 2>&1 && true
+kill $( lsof -i:12222-12223 -t ) > /dev/null 2>&1 && true 
+for i in $(seq 0 $(($num_workers - 1))); do
+  docker kill w-$i
+done
+
+echo Creating tmux session and windows...
+tmux kill-session -t $session && true
+tmux new-session -s $session -n ps -d bash
+tmux new-window -t $session -n tb bash
+tmux new-window -t $session -n htop bash
+for i in $(seq 0 $(($num_workers - 1))); do
+  tmux new-window -t $session -n w-$i bash
+done
+sleep 1
+
+echo Executing commands in TMUX
+tmux send-keys -t a3c:ps \
+  "CUDA_VISIBLE_DEVICES= /usr/bin/python worker.py "\
+  "--log-dir $logdir --env-id gazebo --num-workers $NUM_WORKERS --job-name ps"\
+  Enter
+for i in $(seq 0 $(($num_workers - 1))); do
+  worker_args="--logdir $logdir\
+               --env-id gazebo\
+               --num-workers $num_workers\
+               --task $i\
+               --remote 1\
+               --spec '$spec'"
+  tmux send-keys -t a3c:w-$i \
+  "docker run -it --rm --name=w-0 --net=host ardrone /start.sh     false \"$worker_args\"" Enter
+  #                                       start script  gui
+done
+tmux send-keys -t a3c:tb 'tensorboard --logdir ardrone --port 12345' Enter
+tmux send-keys -t a3c:htop 'htop' Enter
+
+echo 'Use `tmux attach -t a3c` to watch process output
+Use `tmux kill-session -t a3c` to kill the job
+Point your browser to http://localhost:12345 to see Tensorboard'
