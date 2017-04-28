9419d231db35de4e84ef0b04d793f88217efb5e2
Ethan Brooks <ethanabrooks@gmail.com>
2017-04-28 18:01:15 -0400

    added ardrone.sh

---
+++ ardrone.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+echo ------------------------------------------------------------------
+echo Xvfb :1 -screen 0 1600x1200x16  &
+Xvfb :1 -screen 0 1600x1200x16  &
+echo ------------------------------------------------------------------
+echo source /opt/ros/kinetic/setup.bash; source /catkin/devel/setup.bash; roscd a3c
+source /opt/ros/kinetic/setup.bash; source /catkin/devel/setup.bash; roscd a3c
+echo ------------------------------------------------------------------
+echo roslaunch a3c train.launch gui:=$1 worker-args:=\"$2\"
+roslaunch a3c train.launch gui:=$1 worker-args:="$2"
+echo ------------------------------------------------------------------
