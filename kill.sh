a34ae8ea3339ec1f569e5a29a402c2284096f97f
Ethan Brooks <ethanabrooks@gmail.com>
2017-05-12 14:22:40 -0400

    made a3c default arg to kil.sh

--- kill.sh
+++ kill.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-# TODO: make a3c a default arg
-if [[ $# != 1 ]]; then
-  echo 'usage: kill.sh [SESSION_NAME]'
-  exit
-fi
+session=${1:-a3c}
 
 # kill processes using ports
 kill $( lsof -i:12345 -t ) > /dev/null 2>&1
@@ -21,7 +17,7 @@ if [[ ! -z "$ps" ]]; then
   docker kill $ps
 fi
 
-if [[ ! -z $(tmux list-session | grep $1) ]]; then
-  echo Killing previous $1 session
-  tmux kill-session -t $1
+if [[ ! -z $(tmux list-session | grep $session) ]]; then
+  echo Killing previous $session session
+  tmux kill-session -t $session
 fi
